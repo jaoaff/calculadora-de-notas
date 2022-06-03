c251fbb57e07c80e74f9f2848c269eb792102316
jaoaff <101889176+jaoaff@users.noreply.github.com>
2022-06-02 22:59:09 -0300

    commit incial
    adição dos arquivos base

---
+++ media.js
@@ -0,0 +1,21 @@
+function calcular(){
+	const p1 = Number(document.getElementById("P1").value);
+	const peso= Number(document.getElementById("peso").value);
+	const outros= Number(document.getElementById("outros").value);
+	const quiz= Number(document.getElementById("quiz").value);
+
+
+	let total = outros + quiz;
+
+	let sobra = 7 - total;
+
+	let conta = (sobra * 10) / peso;
+
+	let result = (conta * 2) - p1;
+
+
+
+
+	const exibirResultado = document.getElementById("resultado");
+	exibirResultado.innerHTML = `Para ter uma média final igual a 7 você pecisa tirar ${parseFloat(result.toFixed(1))}`;
+}
